d245f7e35fa7ee93c226b83bf2ed9f2953088a0e
KCSNET\st20214139 <Saotome.myato@gmail.com>
2024-06-24 11:22:09 +0900

    no message

--- src/main/webapp/js/shiftRegister.js
+++ src/main/webapp/js/shiftRegister.js
@@ -1,5 +1,5 @@
-import { DAYS } from './const.js';
-import { DAY_TEXTS } from './const.js';
+import { DAYS } from "./const.js";
+import { DAY_TEXTS } from "./const.js";
 
 // export const DAYS = {
 // 	SUNDAY: 1,
@@ -21,52 +21,86 @@ import { DAY_TEXTS } from './const.js';
 // 	[DAYS.SATURDAY]: "土",
 // }
 
-const dayElements = {};
+const dayElements = [];
 
 const selectWeekEl = document.getElementById("selectWeek");
 const inputTimeGroupEl = document.getElementById("inputTimeGroup");
 // console.log("a: ", inputTimeGroupEl)
 
-for(const day of Object.values(DAYS)) {
-	console.log("day:", day)
-	const div = document.createElement("div");
-	const daybtn = document.createElement("button");
-	daybtn.innerText = DAY_TEXTS[day];
-	daybtn.value = day;
-	div.appendChild(daybtn);
-	selectWeekEl.appendChild(div);
+for (const day of Object.values(DAYS)) {
+  console.log("day:", day);
+  const div = document.createElement("div");
+  const daybtn = document.createElement("button");
+  daybtn.innerText = DAY_TEXTS[day];
+  daybtn.value = day;
+  div.appendChild(daybtn);
+  selectWeekEl.appendChild(div);
 
-	daybtn.addEventListener("click", function(event){
-		const selectValue = event.target.value;
-		if(document.getElementById(selectValue) !== null){
-			const remove_div = document.getElementById(selectValue);
-			// daybtn.classList.remove("gray");
-			remove_div.remove();
-		}else{
-			const dayTextEl = document.createElement("div");
-			dayTextEl.id = selectValue;
-			const dayTextPr = document.createElement("p");
-			dayTextPr.innerText = DAY_TEXTS[selectValue];
-			console.log(dayTextEl);
-			console.log("selectValue:",selectValue);
+  daybtn.addEventListener("click", function (event) {
+    const selectValue = event.target.value;
 
-			const startTime = document.createElement("input");
-			startTime.type = "text";
-			const endTime = document.createElement("input");
-			endTime.type = "text";
-			dayTextEl.appendChild(dayTextPr);
-			dayTextEl.appendChild(startTime);
-			dayTextEl.appendChild(endTime);
-			
-			
-			inputTimeGroupEl.appendChild(dayTextEl);
-			// daybtn.classList.add("gray");
-		}
-		daybtn.classList.toggle("gray");
-	})
-
-	
+    const index = dayElements.findIndex((x) => x === selectValue);
+    if (index === -1) {
+      dayElements.push(selectValue);
+    } else {
+      dayElements.splice(index, 1);
+    }
+    dayElements.sort(function (first, second) {
+      if (first > second) {
+        return 1;
+      } else if (first < second) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    daybtn.classList.toggle("gray");
+    showDayElements();
+  });
 }
 
-// DAYS.forEach((day) => {
-// })
+function showDayElements() {
+  while (inputTimeGroupEl.firstChild) {
+    inputTimeGroupEl.removeChild(inputTimeGroupEl.firstChild);
+  }
+  for (let i = 0; i < dayElements.length; i++) {
+    const selectValue = dayElements[i];
+    const dayTextEl = document.createElement("div");
+    dayTextEl.id = `item_${selectValue}`;
+    const dayTextPr = document.createElement("p");
+    dayTextPr.innerText = DAY_TEXTS[selectValue];
+    console.log(dayTextEl);
+    console.log("selectValue:", selectValue);
+
+    const startTimeBox = document.createElement("input");
+    startTimeBox.type = "time";
+    startTimeBox.classList.add("startTime");
+    const endTimeBox = document.createElement("input");
+    endTimeBox.type = "time";
+    endTimeBox.classList.add("endTime");
+    dayTextEl.appendChild(dayTextPr);
+    dayTextEl.appendChild(startTimeBox);
+    dayTextEl.appendChild(endTimeBox);
+
+    inputTimeGroupEl.appendChild(dayTextEl);
+  }
+}
+
+const btn = document.createElement("button");
+//btn.type = submit;
+btn.innerText = "送信";
+btn.addEventListener("click", function (event) {
+  console.log("送信しました");
+  const children = inputTimeGroupEl.children;
+
+  for (let i = 0; i < children.length; i++) {
+    const id = children[i].id;
+    console.log("id: ", id);
+    // const startTime = document.querySelector(`#1`);
+    const startTime = document.getElementById("item_1");
+    console.log("startTime: ", startTime);
+  }
+});
+document.body.appendChild(btn);
+
+const shiftDay = {};
